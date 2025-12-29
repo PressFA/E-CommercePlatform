21f2d545ca56637247f838a17d5b90b5ffd6adac
PressF <artemsurmenok@gmail.com>
2025-12-30 00:06:56 +0300

    First master commit

---
+++ PaymentMS/src/main/java/by/pressf/paymentms/PaymentMsApplication.java
@@ -0,0 +1,13 @@
+package by.pressf.paymentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMsApplication.class, args);
+    }
+
+}
